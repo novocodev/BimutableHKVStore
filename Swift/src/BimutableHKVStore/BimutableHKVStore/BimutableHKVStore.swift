f1c2d2a553dc9bf86875f0519bbe46510239c9fc
Novoco Dev <dev@xn--voo-zpa.com>
2016-07-27 10:30:19 +0100

    Added optional completion callbacks to method signatures

--- Swift/src/BimutableHKVStore/BimutableHKVStore/BimutableHKVStore.swift
+++ Swift/src/BimutableHKVStore/BimutableHKVStore/BimutableHKVStore.swift
@@ -12,17 +12,17 @@ import Result
 
 protocol BimutableHKVStore {
     
-    func addBlock(block: Array<UInt8>) -> Future<Optional<Multihash>, NoError>;
+    func addBlock(block: Array<UInt8>, completion: Optional<(key: Multihash) -> Void>) -> Future<Optional<Multihash>, NoError>;
 
-    func addBlocks(blocks: Array<Array<UInt8>>) -> Future<Optional<Array<Multihash>>, NoError>;
+    func addBlocks(blocks: Array<Array<UInt8>>, completion: Optional<(keys: Array<Multihash>) -> Void>) -> Future<Optional<Array<Multihash>>, NoError>;
 
-    func getBlock(key: Multihash) -> Future<Optional<Array<UInt8>>, NoError>;
+    func getBlock(key: Multihash, completion: Optional<(value: Array<UInt8>) -> Void>) -> Future<Optional<Array<UInt8>>, NoError>;
 
-    func getBlocks(keys: Array<Multihash>) -> Future<Optional<Array<Array<UInt8>>>, NoError>;
+    func getBlocks(keys: Array<Multihash>, completion: Optional<(values: Array<Array<UInt8>>) -> Void>) -> Future<Optional<Array<Array<UInt8>>>, NoError>;
 
-    func deleteBlock(key: Multihash);
+    func deleteBlock(key: Multihash) -> Void;
 
-    func resolve(key: Multihash) -> Future<Optional<Array<Array<UInt8>>>, NoError>;
+    func resolve(key: Multihash, completion: Optional<(values: Array<Array<UInt8>>) -> Void>) -> Future<Optional<Array<Array<UInt8>>>, NoError>;
     
-    func publish(secret: Array<UInt8>, rootBlockKey: Multihash) ->  Future<Optional<Multihash>, NoError>;
+    func publish(secret: Array<UInt8>, rootBlockKey: Multihash, completion: Optional<(key: Multihash) -> Void>) ->  Future<Optional<Multihash>, NoError>;
 }
